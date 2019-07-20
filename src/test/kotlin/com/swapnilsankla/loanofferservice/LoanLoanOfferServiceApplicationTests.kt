e378e540995ce5710be9ade1ca94f018acbedb46
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-07-20 09:49:39 +0530

    Use embedded kafka in tests

--- src/test/kotlin/com/swapnilsankla/loanofferservice/LoanLoanOfferServiceApplicationTests.kt
+++ src/test/kotlin/com/swapnilsankla/loanofferservice/LoanLoanOfferServiceApplicationTests.kt
@@ -1,16 +1,26 @@
 package com.swapnilsankla.loanofferservice
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.swapnilsankla.loanofferservice.listener.NeedLoanEvent
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@EmbeddedKafka(partitions = 1,
+        controlledShutdown = false,
+        brokerProperties = ["listeners=PLAINTEXT://localhost:3333", "port=3333"])
+@TestPropertySource(properties = [
+    "spring.kafka.bootstrap-servers=\${spring.embedded.kafka.brokers}"
+])
 class LoanLoanOfferServiceApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
