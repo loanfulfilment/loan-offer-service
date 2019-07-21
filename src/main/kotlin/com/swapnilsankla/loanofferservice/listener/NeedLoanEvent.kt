c8dad073d21dbfd5efb2a1a6fdd2d4b9ba4f3974
Swapnil Sankla <swapnil.sankla@gmail.com>
2019-07-21 19:24:13 +0530

    Application number for the requested loan

--- src/main/kotlin/com/swapnilsankla/loanofferservice/listener/NeedLoanEvent.kt
+++ src/main/kotlin/com/swapnilsankla/loanofferservice/listener/NeedLoanEvent.kt
@@ -1,3 +1,3 @@
 package com.swapnilsankla.loanofferservice.listener
 
-data class NeedLoanEvent(val customerId: String)
+data class NeedLoanEvent(val customerId: String, val applicationNumber: String)
